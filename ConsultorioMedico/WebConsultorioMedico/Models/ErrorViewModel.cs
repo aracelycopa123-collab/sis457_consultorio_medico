599b00102603d703e0f41437dc0f58c25a4673d3
Soledad <aracelycopa123@gmail.com>
2025-11-22 14:30:38 -0400

    feat: creación del archivo y controladores

---
+++ ConsultorioMedico/WebConsultorioMedico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebConsultorioMedico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
