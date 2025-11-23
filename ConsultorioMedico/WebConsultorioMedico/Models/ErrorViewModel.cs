b1ad3bb8ae9212a0720607a30ae72ae46f4fde82
Soledad Copa <aracelycopa123@gmail.com>
2025-11-23 19:08:51 -0400

    se actualizara de nuevo

--- ConsultorioMedico/WebConsultorioMedico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebConsultorioMedico.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
