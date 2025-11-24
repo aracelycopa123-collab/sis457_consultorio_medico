ecfde7976d85bee5f309e1be5702b45238066dd7
Mabel <colquefigueroamaribeljovita@gmail.com>
2025-11-24 18:29:36 -0400

    Update Program.cs

--- Sis457ConsultorioMedico/Sis457ConsultorioMedico/Program.cs
+++ Sis457ConsultorioMedico/Sis457ConsultorioMedico/Program.cs
@@ -26,7 +26,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseCookiePolicy();
 app.UseSession();
 app.UseAuthentication();
