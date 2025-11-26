9036e554980ad17bb4e135281b948597f4f34689
Mabel <colquefigueroamaribeljovita@gmail.com>
2025-11-26 12:33:47 -0400

    HomeController actualizado
    Removed Index, Privacy, and Error actions from HomeController.

--- Sis457ConsultorioMedico/Sis457ConsultorioMedico/Controllers/HomeController.cs
+++ Sis457ConsultorioMedico/Sis457ConsultorioMedico/Controllers/HomeController.cs
@@ -13,20 +13,4 @@ namespace Sis457ConsultorioMedico.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
-}
